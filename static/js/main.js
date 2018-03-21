a732b03dc19d8d9c7f2686ac796d845348b5e716
Matt Belisle <matthew.belisle@workiva.com>
2018-03-21 14:44:07 -0500

    Fixing spacing

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 function update() {
-    let matches1 = new Set($('#textarea-1').val().match(/(\d+)/g));;
-    let matches2 = new Set($('#textarea-2').val().match(/(\d+)/g));;
+    let matches1 = new Set($('#textarea-1').val().match(/(\d+)/g));
+    let matches2 = new Set($('#textarea-2').val().match(/(\d+)/g));
     $('#textarea-1-readonly').val([...matches1].filter(x => !matches2.has(x)));
     $('#textarea-2-readonly').val([...matches2].filter(x => !matches1.has(x)));
 }
